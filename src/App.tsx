18ee0b03e752925acda12ed08464a3b53eb348e4
Bohdan Hora <voidinvok@gmail.com>
2024-06-01 06:18:19 +0300

    Install "react-router", added default use colors.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <header className="text-2xl bold">Fist commit</header>
+      <header className="text-2xl bold text-gray-text">Fist commit</header>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'gray-text': '#BDBDBD',
+      'light-gray': '#E5E5E5',
+      'dark-gray': '#33333',
+      'background-gray': '#2C2C2C',
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
